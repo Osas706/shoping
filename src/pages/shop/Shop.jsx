278da1fa29c705ca69d1174a270839ab032f9450
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-02-26 18:19:38 +0100

    shoping

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='shop'>
+      <div className='shopTitle'>
+        <h1>Winn Store</h1>
+      </div>
+
+      <div className="products">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Shop
